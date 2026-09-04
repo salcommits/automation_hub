d41c4ef993c6d829ee705ccdfcec1aa3f47678f5
Liam Atkins <liam@carnoglobal.com>
2026-09-04 10:53:41 +0100

    Initial Commit

---
+++ automationhub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  automationhub
+//
+//  Created by Liam Atkins on 04/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ automationhub/automationhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  automationhubApp.swift
+//  automationhub
+//
+//  Created by Liam Atkins on 04/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct automationhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
